bc5ad8d65e946d5c0f02002cfedb7a77d9724ef9
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-06-14 15:50:37 +0100

    Adds Random PC move

--- src/game.js
+++ src/game.js
@@ -4,14 +4,20 @@ function Game(){
     'paper': 'rock',
     'scissors': 'paper'
     };
+  this.options = ["Rock","Paper","Scissors"]
 };
 
-Game.prototype.play = function(userMove,randomMove) {
-  if(userMove === randomMove) {
+Game.prototype.play = function(userMove,computerMove) {
+  if(userMove === computerMove) {
     return "DRAW";
-    } else if(this.WIN_CONDITIONS[userMove] === randomMove) {
+    } else if(this.WIN_CONDITIONS[userMove] === computerMove) {
       return "WIN"
     } else {
       return "LOSE";
     };
+};
+
+Game.prototype.computerMove = function(){
+  var randomChoice = this.options[Math.floor(Math.random()*this.options.length)];
+  return randomChoice;
 };
